9604f83e9739f711eb5bfae28e3d89de0471c018
we125182 <we1215107007@163.com>
2022-02-09 17:10:02 +0800

    init: 项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <span>{{ msg }}</span>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
